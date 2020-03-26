14f28b15c99731976147258fd19b9f21cd1005dd
FrcGustavo <hidalgo_fco520@outlook.com>
2020-03-26 14:31:19 -0600

    Setup and first components

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,19 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
-  </div>
+  <main>
+    <px-header />
+    <px-assets-table />
+  </main>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
+import PxHeader from "@/components/PxHeader";
+import PxAssetsTable from "@/components/PxAssetsTable";
 
 export default {
   name: "App",
   components: {
-    HelloWorld
+    PxHeader,
+    PxAssetsTable
   }
 };
 </script>
