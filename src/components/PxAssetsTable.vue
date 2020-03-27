5853f3063938ca9765dff139ed067718e7b026d2
FrcGustavo <hidalgo_fco520@outlook.com>
2020-03-26 18:52:15 -0600

    add filters and to get better UI

--- src/components/PxAssetsTable.vue
+++ src/components/PxAssetsTable.vue
@@ -21,6 +21,7 @@
       >
         <td>
           <img
+            class="w-6 h-6"
             :src="
               `https://static.coincap.io/assets/icons/${a.symbol.toLowerCase()}@2x.png`
             "
@@ -31,9 +32,15 @@
           <b># {{ a.rank }}</b>
         </td>
         <td>{{ a.name }}</td>
-        <td>{{ a.priceUsd }}</td>
+        <td>{{ a.priceUsd | dollar }}</td>
         <td>{{ a.marketCapUsd }}</td>
-        <td>{{ a.changePercent24Hr }}</td>
+        <td
+          :class="
+            a.changePercent24Hr.includes('-') ? 'tex-red-600' : 'text-green-600'
+          "
+        >
+          {{ a.changePercent24Hr | percent }}
+        </td>
         <td class="hidden sm:block"></td>
       </tr>
     </tbody>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,11 @@
 import Vue from "vue";
 import App from "./App.vue";
 import router from "@/router";
+import { dollarFilter, percentFilter } from "@/filters";
 import "@/assets/css/tailwind.css";
 
+Vue.filter("dollar", dollarFilter);
+Vue.filter("percent", percentFilter);
 Vue.config.productionTip = false;
 
 new Vue({
