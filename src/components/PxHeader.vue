fdb59d324c415ffe5af46c08ffdeea3b7b7942d0
FrcGustavo <hidalgo_fco520@outlook.com>
2020-03-26 19:31:22 -0600

    programmatic nav

--- src/components/PxHeader.vue
+++ src/components/PxHeader.vue
@@ -4,9 +4,9 @@
       <nav class="flex items-center justify-between flex-wrap bg-green-400 p-6">
         <div class="flex items-center flex-shrink-0 text-white mr-6">
           <px-icon class="mr-2" />
-          <span class="font-semibold text-xl tracking-tight"
-            >PlatziExchange</span
-          >
+          <router-link class="font-semibold text-xl tracking-tight" to="/">
+            PlatziExchange
+          </router-link>
         </div>
         <div
           class="hidden sm:block w-full block flex-grow lg:flex lg:items-center lg:w-auto"
