9429325662fdcc6b3e3ffc29e697109c60600b6e
FrcGustavo <hidalgo_fco520@outlook.com>
2020-03-26 18:34:41 -0600

    Use fetch api

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,15 +1,26 @@
 <template>
   <div>
-    <px-assets-table />
+    <px-assets-table v-bind:assets="assets" />
   </div>
 </template>
 
 <script>
 import PxAssetsTable from "@/components/PxAssetsTable";
+import api from "@/api";
 export default {
   name: "Home",
   components: {
     PxAssetsTable
+  },
+
+  data() {
+    return {
+      assets: []
+    };
+  },
+
+  created() {
+    api.getAssets().then(assets => (this.assets = assets));
   }
 };
 </script>
